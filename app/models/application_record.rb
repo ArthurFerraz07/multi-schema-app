630cacd0e87e6f3e5f823a1429d2c792555e142f
Arthur <arthur.ferraz07@hotmail.com>
2021-03-22 11:03:37 -0300

    .

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,19 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  def schema_valid?
+    current_schema = self.class.table_name.split('.').first
+    if current_schema.blank? || current_schema == self.class.table_name || Rails.application.database_schemas.exclude?(current_schema)
+      errors.add(:base, :invalid)
+      throw :abort
+    end
+    true
+  end
+
+  def self.change_schema(schema = '')
+    return nil if Rails.application.database_schemas.exclude?(schema)
+
+    klass = clone
+    klass.table_name = "#{schema}.cars"
+    klass
+  end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,3 @@
 class Car < ApplicationRecord
-  def self.change_schema(schema = '')
-    return nil if Rails.application.database_schemas.exclude?(schema)
-
-    klass = clone
-    klass.table_name = "#{schema}.cars"
-    klass
-  end
+  validate :schema_valid?
 end
